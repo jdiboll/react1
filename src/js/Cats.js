32be48714b1adbddacdcb8cc9681c3f7fc67f086
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-29 13:07:23 -0400

    class Cats added
    Html element moved to Cats.
    "/" added at the end of image tags
    "class" changed to "className"

---
+++ src/js/Cats.js
@@ -0,0 +1,47 @@
+export default class Cats extends React.Component {
+render() {
+  return (
+    <div class="dex">
+      <div class="octo">
+        <img src="https://octodex.github.com/images/nyantocat.gif">
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/welcometocat.png"/>
+      
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/filmtocat.png"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/privateinvestocat.jpg"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/gobbleotron.gif"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/maxtocat.gif"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/mummytocat.gif"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/daftpunktocat-thomas.gif"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/spidertocat.png"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/stormtroopocat.png"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/heisencat.png"/>
+      </div>
+      <div className="octo">
+        <img src="https://octodex.github.com/images/grim-repo.jpg"/>
+      </div>
+    );
+  }
+}
+
+
+
